c117da6200e0a0b7a09176a06ab0a29a3a2bf055
Eliana Levenson <elianalevenson@Elianas-MacBook-Air.local>
2019-04-11 13:48:43 -0700

    update some styling

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -7,30 +7,34 @@ const ProfileInfo = (props) => {
                 <h1>{props.user.name}</h1>
                 <h5>about me</h5>
                 <p>{props.user.bio}</p>
-                <h5>my trigger words</h5>
             </div>
-            <div className='Profile-triggerwords-container'>
-                {props.user.triggerwords.map(word => 
-                    <button
-                        key={word}
-                        name={word}
-                        className='Profile-triggerwords'
-                        onClick={props.handleRemoveTrigger}
-                    >
-                    {word}
-                    </button>
-                )}
-                <h5>update trigger words</h5>
+            <div className='Profile-wrapper'>
+                <h4>my trigger words</h4>
                 <div className='Profile-triggerwords-container'>
-                {props.triggerWords.map(word =>
-                    <button
-                        key={word}
-                        name={word}
-                        className='Profile-triggerword-button'
-                        onClick={props.handleAddTrigger}
-                    >
-                    {word}
-                    </button>)}
+                    {props.user.triggerwords.map(word => 
+                        <button
+                            key={word}
+                            name={word}
+                            className='Profile-triggerwords'
+                            onClick={props.handleRemoveTrigger}
+                        >
+                        {word}
+                        </button>
+                    )}
+                </div>
+            </div>
+            <div className='Profile-wrapper'>
+                <h4>update trigger words</h4>
+                <div className='Profile-triggerwords-container'>
+                    {props.triggerWords.map(word =>
+                        <button
+                            key={word}
+                            name={word}
+                            className='Profile-triggerword-button'
+                            onClick={props.handleAddTrigger}
+                        >
+                        {word}
+                        </button>)}
                 </div>
             </div>
         </div>
